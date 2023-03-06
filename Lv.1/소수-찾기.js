d6fbfab8b4adf0eb44a24437db3f5a9da5560e6d
Kim Hayeon <k.hayeon00@gmail.com>
2023-03-06 23:27:59 +0900

    소수 찾기 풀이

---
+++ Lv.1/소수-찾기.js
@@ -0,0 +1,13 @@
+function solution(n) {
+  const arr = [...Array(n-1).keys()].map(v => v+2);
+  for (let i = 2; i < Math.sqrt(n); i++) {
+    if (arr[i-2] !== null) {
+      for (let j = 2; i * j <= n; j++) {
+        arr[i * j - 2] = null;
+      }
+    }
+  }
+  return arr.filter(v => v !== null).length;
+}
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/12921
