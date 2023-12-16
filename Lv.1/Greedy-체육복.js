94291c6828478532e2d8c6eb3e9c574b9948238e
Kim Hayeon <108985221+KimHayeon1@users.noreply.github.com>
2023-12-16 14:34:28 +0900

    Update Greedy-체육복.js

--- Lv.1/Greedy-체육복.js
+++ Lv.1/Greedy-체육복.js
@@ -1,18 +1,34 @@
 function solution(n, lost, reserve) {
-  const filter_reserve = reserve.filter(v => !lost.includes(v));
-  const filter_lost = lost.filter(v => !reserve.includes(v));
+    const filter_lost = [...lost];
+    const filter_reserve = reserve.filter(v => {
+        const lostIndex = filter_lost.indexOf(v);
+        
+        if (lostIndex === -1) {
+            return true;
+        } else {
+            filter_lost.splice(lostIndex, 1); 
+            return false;
+        }
+    }).sort();
+    filter_lost.sort();
   
-  filter_lost.sort((a, b) => a - b);
-  filter_reserve.sort((a, b) => a - b);
-  
-  filter_reserve.forEach(v => {
-      if(filter_lost.includes(v-1)) {
-          filter_lost.splice(filter_lost.indexOf(v-1), 1);
-      } else if (filter_lost.includes(v+1)) {
-          filter_lost.splice(filter_lost.indexOf(v+1), 1);
-      }
-  });
-  return n - filter_lost.length;
+    filter_reserve.forEach(v => {
+        const beforeIndex = filter_lost.indexOf(v-1);
+        
+        if (beforeIndex !== -1) {
+            filter_lost.splice(beforeIndex, 1);  
+            return;
+        }
+
+        const afterIndex = filter_lost.indexOf(v+1);
+        
+        if (afterIndex !== -1) {
+            filter_lost.splice(afterIndex, 1);  
+            return;
+        }
+    });
+    
+    return n - filter_lost.length;
 }
 
 // https://school.programmers.co.kr/learn/courses/30/lessons/42862
