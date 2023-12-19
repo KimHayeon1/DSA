f6bbd18568713344d84b0fe1fcb8dad6814ee2d4
Kim Hayeon <108985221+KimHayeon1@users.noreply.github.com>
2023-12-19 15:43:35 +0900

    Update 2.js

--- [JS풀이]이것이-코딩-테스트다/3/2.js
+++ [JS풀이]이것이-코딩-테스트다/3/2.js
@@ -1,3 +1,4 @@
+// 단순하게 푸는 답안
 let [n, m, k] = prompt().split(' ').map(str => parseInt(str));
 const data = prompt().split(' ').map(str => parseInt(str));
 
@@ -24,5 +25,22 @@ while (true) {
   result += second;
   m--;
 }
+
+console.log(result);
+
+// 답안
+const [n, m, k] = prompt().split(' ').map(str => parseInt(str));
+const data = prompt().split(' ').map(str => parseInt(str));
+
+data.sort();
+const first = data[n - 1];
+const second = data[n - 2];
+
+let count = parseInt(m / (k + 1)) * k;
+count += m % (k + 1);
+
+let result = 0;
+result += count * first;
+result += (m - count) * second;
 
 console.log(result);
