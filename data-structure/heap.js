11f3439e3ae7c10539cbe17b6745cbfb5e2da472
Kim Hayeon <108985221+KimHayeon1@users.noreply.github.com>
2023-12-31 14:01:53 +0900

    Create heap.js

---
+++ data-structure/heap.js
@@ -0,0 +1,63 @@
+class MinHeap {
+  constructor() {
+    this.heap = [null];
+  }
+ 
+  getMin() {
+    return this.heap[1];
+  }
+ 
+  getSize() {
+    return this.heap.length - 1;
+  }
+ 
+  isEmpty() {
+    return this.heap.length < 2;
+  }
+  
+  insert(node) {
+    let current = this.heap.length;
+
+    while (current > 1 && this.heap[Math.trunc(current / 2)] > node) {
+      this.heap[current] = this.heap[Math.trunc(current / 2)];
+      current = Math.trunc(current / 2);
+    }
+
+    this.heap[current] = node;
+  }
+  
+  delete() {      
+    if (this.heap.length === 1) {
+      return null;
+    }
+
+    const min = this.heap[1];
+
+    if (this.heap.length === 2) {
+      this.heap.splice(1);
+      return min;
+    }
+
+    this.heap[1] = this.heap[this.heap.length - 1];
+    this.heap.splice(-1);
+
+    let parent = 1;
+    let child = 2;
+  
+    while (child <= this.getSize()) {      
+      if (this.heap[child + 1] && this.heap[child + 1] < this.heap[child]) {
+        child++;
+      }
+  
+      if (this.heap[parent] <= this.heap[child]) {
+        break;
+      }
+
+      [this.heap[parent], this.heap[child]] = [this.heap[child], this.heap[parent]];
+      parent = child;
+      child *= 2;
+    }
+
+    return min;
+  }
+}
