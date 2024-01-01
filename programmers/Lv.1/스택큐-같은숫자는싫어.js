2d73f36e7ea46ea4a490cc865718f4667d3137dc
Kim Hayeon <108985221+KimHayeon1@users.noreply.github.com>
2024-01-01 18:56:19 +0900

    Update 스택큐-같은숫자는싫어.js

--- programmers/Lv.1/스택큐-같은숫자는싫어.js
+++ programmers/Lv.1/스택큐-같은숫자는싫어.js
@@ -1,3 +1,17 @@
 function solution(arr){
   return arr.filter((v, i) => v !== arr[i + 1]);
 }
+
+function solution(arr) {
+    const answer = [arr[0]];
+
+    for (let i = 1; i < arr.length; i++) {
+        if (answer[answer.length - 1] !== arr[i]) {
+            answer.push(arr[i]);
+        }
+    }
+
+    return answer;
+}
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/12906
