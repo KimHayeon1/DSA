b68cad2c46df8be8e7b50c3030f4ddbd7e93a293
Kim Hayeon <108985221+KimHayeon1@users.noreply.github.com>
2024-01-04 14:54:14 +0900

    Update 정렬-K번째수.js

--- programmers/Lv.1/정렬-K번째수.js
+++ programmers/Lv.1/정렬-K번째수.js
@@ -1,10 +1,12 @@
 function solution(array, commands) {
   const answer = [];
+  
   commands.forEach(command => {
-      const arr = array.slice(command[0]-1, command[1])
-          .sort((a, b) => a - b);
-      answer.push(arr[command[2]-1]);
+      answer.push(
+        array.slice(command[0]-1, command[1]).sort((a, b) => a - b)[command[2]-1]
+      );
   })
+  
   return answer;
 }
 
