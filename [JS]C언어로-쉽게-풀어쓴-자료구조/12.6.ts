7c3507098e671c8332a8d53b1982124168feed2b
Kim Hayeon <108985221+KimHayeon1@users.noreply.github.com>
2024-01-17 15:55:31 +0900

    합병 정렬 파일 추가

---
+++ [JS]C언어로-쉽게-풀어쓴-자료구조/12.6.ts
@@ -0,0 +1,45 @@
+const sorted: number[] = [];
+
+function merge(list: number[], left: number, mid: number, right: number) {
+  let i: number, j: number, k: number, l: number;
+  i = left;
+  j = mid + 1;
+  k = left;
+
+  while (i <= mid && j <= right) {
+    if (list[i] <= list[j]) {
+      sorted[k++] = list[i++];
+    } else {
+      sorted[k++] = list[j++];
+    }
+  }
+
+  if (i > mid) {
+    for (l = j; l <= right; l++) {
+      sorted[k++] = list[l];
+    }
+  } else {
+    for (l = i; l <= mid; l++) {
+      sorted[k++] = list[l];
+    }
+  }
+
+  for (l = left; l <= right; l++) {
+    list[l] = sorted[l];
+  }
+}
+
+function mergeSort(list: number[], left: number, right: number) {
+  let mid: number;
+
+  if (left < right) {
+    mid = (left + right) / 2;
+    mergeSort(list, left, mid);
+    mergeSort(list, mid + 1, right);
+    merge(list, left, mid, right);
+  }
+}
+
+// test
+const array = [27, 10, 12, 20, 25, 13, 15, 22];
+mergeSort(array, 0, 7);
