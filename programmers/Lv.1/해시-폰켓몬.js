5d9d4cef8bb236c6e8c84c50b6f5c3c334135d93
Kim Hayeon <108985221+KimHayeon1@users.noreply.github.com>
2024-06-08 14:43:49 +0900

    Update 해시-폰켓몬.js

--- programmers/Lv.1/해시-폰켓몬.js
+++ programmers/Lv.1/해시-폰켓몬.js
@@ -1,7 +1,8 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/1845
+
+// Set(O(N)보다 빠른 해시(O(1)) 테이블, 검색 트리(O(log(N)), 혹은 다른 자료구조)
 function solution(nums) {
   const half = nums.length / 2;
   const set = new Set(nums).size;
   return half < set ? half : set;
 }
-
-// https://school.programmers.co.kr/learn/courses/30/lessons/1845
