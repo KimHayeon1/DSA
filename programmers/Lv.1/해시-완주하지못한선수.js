31954937b06c83a620c02fd80846da470db60b74
Kim Hayeon <108985221+KimHayeon1@users.noreply.github.com>
2024-06-08 15:22:54 +0900

    Update 해시-완주하지못한선수.js

--- programmers/Lv.1/해시-완주하지못한선수.js
+++ programmers/Lv.1/해시-완주하지못한선수.js
@@ -1,12 +1,21 @@
-function solution(participant, completion) {
-  const obj = {};
-  participant.forEach(v => {
-      obj[v] ? obj[v]++ : obj[v] = 1;
-  });
-  completion.forEach(v => {
-      obj[v]--;
-  })
-  return Object.keys(obj).find((key) => obj[key] === 1)
-}
-
 // https://school.programmers.co.kr/learn/courses/30/lessons/42576
+
+// Map
+function solution(participant, completion) {
+    const map = new Map();
+
+    participant.forEach((v, i) => {
+        map.set(v, (map.get(v) || 0) + 1);
+        
+        if (i < completion.length) {
+            const name = completion[i];
+            map.set(name, (map.get(name) || 0) - 1);
+        }
+    });
+    
+    for (const [key, value] of map) {
+        if (value === 1) {
+            return key;
+        }
+    }
+}
