7036ee186e2bc8db999faef23f3c35ca02d81942
Kim Hayeon <108985221+KimHayeon1@users.noreply.github.com>
2024-06-18 16:05:04 +0900

    Update 8.js

--- [JS]이것이-코딩-테스트다/5/8.js
+++ [JS]이것이-코딩-테스트다/5/8.js
@@ -24,3 +24,12 @@ const graph = [
 const visited = Array(9).fill(false);
 
 dfs(graph, 1, visited);
+
+// 1 -> [2, 3, 8] [false, true, false * 7]
+// 2 -> [1, 7] [false, true * 2, false * 6]
+// 7 -> [2, 6, 8] [false, true * 2, false * 4, true, false]
+// 6 -> [7] [false, true * 2, false * 3, true * 2, false]
+// 8 -> [1, 7] [false, true * 2, false * 3, true * 3]
+// 3 -> [1, 4, 5] [false, true * 3, false * 2, true * 3]
+// 4 -> [3, 5] [false, true * 4, false, true * 3]
+// 5 -> [3, 4] [false, true * 8]
