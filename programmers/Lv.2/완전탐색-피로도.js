498abee1688997ae279562564419f7074a6f2146
KimHayeon <108985221+KimHayeon1@users.noreply.github.com>
2025-06-25 23:05:40 +0900

    Update 완전탐색-피로도.js

--- programmers/Lv.2/완전탐색-피로도.js
+++ programmers/Lv.2/완전탐색-피로도.js
@@ -1,21 +1,28 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/87946?language=javascript
 
-function solution(k, dungeons) {
+const getCases = (arr) => {
     let cases = [[]];
-    let maxCnt = 0;
     
-    dungeons.forEach(v => {
-        const newArr = [];
+    arr.forEach(v => {
+        const newCases = [];
         
         cases.forEach(w => {
             for (let i = 0; i <= w.length; i++) {
-                const temp = w.slice();
-                temp.splice(i, 0, v);
-                newArr.push(temp);
+                const temp = [...w];
+                temp.splice(i, 0, v); // i에 v 삽입
+                newCases.push(temp);
             }
         });
-        cases = newArr;
+        
+        cases = newCases;
     });
+
+    return cases;
+};
+
+const solution = (k, dungeons) => {
+    const cases = getCases(dungeons);
+    let maxCnt = 0;
     
     for (let i = 0; i < cases.length; i++) {
         let cnt = 0;
