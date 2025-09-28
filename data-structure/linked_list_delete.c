57145aed15a81ccb91bd0a6c49e34e3e4e961c20
KimHayeon <108985221+KimHayeon1@users.noreply.github.com>
2025-09-28 21:20:14 +0900

    Create linked_list_delete.c

---
+++ data-structure/linked_list_delete.c
@@ -0,0 +1,25 @@
+void deleteLastNode(linkedList_h* H) {
+    // 리스트에 노드가 없는 경우
+    if (H->head == NULL) {
+        return;
+    }
+
+    // 리스트에 노드가 하나만 있을 경우
+    if (H->head->link == NULL) {
+        free(H->head);
+        H->head = NULL;
+        return;
+    }
+
+    // 마지막에서 두번째 노드 찾기
+    listNode* temp = H->head;
+
+    while (temp->link->link != NULL) {
+        temp = temp->link;
+    }
+
+    // 마지막 노드 삭제
+    listNode* lastNode = temp->link;
+    temp->link = NULL;
+    free(lastNode);
+}
